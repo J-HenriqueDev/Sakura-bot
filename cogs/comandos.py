115cec7db4bf5937a590cdd133fd118765953f0c
sadobiwan <henriquevc8899@gmail.com>
2020-02-28 13:45:55 -0300

    upando

---
+++ cogs/comandos.py
@@ -0,0 +1,76 @@
+﻿import discord
+import requests
+import time
+import datetime
+from io import BytesIO
+from discord.ext import commands
+from PIL import Image, ImageDraw, ImageFont, ImageOps
+
+class comandos(commands.Cog):
+    def __init__(self,bot):
+        self.bot = bot
+
+    @commands.bot_has_permissions(embed_links=True)
+    @commands.command(description='Mostra o meu ping', usage='c.ping')
+    async def ping(self, ctx):
+        embed = discord.Embed(title="🏓 Pong!",
+                              description=f' No Momento estou com: **{round(self.bot.latency * 1000)}ms**.',
+                              color=0x36393f)
+        embed.set_footer(text=self.bot.user.name + " © 2020", icon_url=self.bot.user.avatar_url_as())
+        await ctx.message.delete()
+        await ctx.send(embed=embed, delete_after=90)
+
+
+
+    @commands.command()
+    async def spotify(self, ctx, member: discord.Member = None):
+        if member is None:
+            member = ctx.author
+        act = member.activity
+        if act is None or not act.type == discord.ActivityType.listening:
+            if member == ctx.author:
+                await ctx.send(f'{ctx.author.mention} Você não está ouvindo Spotify.')
+            else:
+                await ctx.send(f'{ctx.author.mention} Esse usuário não está ouvindo Spotify.')
+        else:
+            end = act.end - datetime.datetime.utcnow()
+            end = str(act.duration - end)[2:7]
+            dur = str(act.duration)[2:7]
+            act = member.activity
+            url = requests.get(act.album_cover_url)
+            thumb = Image.open(BytesIO(url.content))
+            thumb = thumb.resize((245, 245));
+            bigsize = (thumb.size[0] * 3, thumb.size[1] * 3)
+            mask = Image.new('L', bigsize, 0)
+            draw = ImageDraw.Draw(mask)
+            draw.ellipse((0, 0) + bigsize, fill=255)
+            mask = mask.resize(thumb.size, Image.ANTIALIAS)
+            thumb.putalpha(mask)
+
+            output = ImageOps.fit(thumb, mask.size, centering=(0.5, 0.5))
+            output.putalpha(mask)
+
+            if len(act.title) >= 21:
+                titulo = act.title[:22]+"..."
+            else:
+                titulo = act.title
+            if len(act.artist) >= 21:
+                cantor = act.artist[:22]+"..."
+            else:
+                cantor = act.artist
+            fundo = Image.open('./files/imagem.png')
+            fonte = ImageFont.truetype('./files/Err Hostess.otf', 35)
+            escrever = ImageDraw.Draw(fundo)
+            escrever.text(xy=(365,150), text=str(titulo),fill=(0,0,0),font=fonte)
+            escrever.text(xy=(360,230), text=str(end + ' - ' + dur),fill=(0,0,0),font=fonte)
+            escrever.text(xy=(365,315), text=str(cantor),fill=(0,0,0),font=fonte)
+            fundo.paste(thumb, (45, 113), thumb)
+            fundo.save('./files/imagem1.png')
+
+            print('enviando')
+            await ctx.send(file=discord.File('./files/imagem1.png'))
+
+    
+
+def setup(bot):
+    bot.add_cog(comandos(bot))
---
+++ main.py
@@ -0,0 +1,89 @@
+import discord
+import os
+import json
+from config import secrets
+from discord.ext import commands
+
+
+
+
+
+class main(discord.ext.commands.Bot):
+    def __init__(self):
+        super().__init__(command_prefix=commands.when_mentioned_or(secrets.PREFIXO),
+                         case_insensitive=True,
+                         pm_help=None,
+                         description="um bot feito por Neo_ para o Servidor da TiaSakura")
+        
+        self.dono = secrets.DONO
+        self.logs = 682655892541276162
+        self.guild = 667532518865502208
+        self.token = 'blz,talvez outro dia.'
+        
+        self.cor = 0xf10cdb
+        self.ecolor = 0xDD2E44
+        self.neutral = 0x36393F
+        
+        for file in [c for c in os.listdir("cogs") if c.endswith(".py")]:
+                name = file[:-3]
+                try:
+                    self.load_extension(f"cogs.{name}")
+                    print(f'MÓDULO [{file}] CARREGADO')
+                except Exception as e:
+                    print(f"FALHA AO CARREGAR  [{file}] MODULO ERROR [{e}]")
+
+
+    async def on_ready(self):
+        print(f"[OK] - {self.user.name} ({self.user.id}) - (Status - Online)")
+        print(f"[OK] - {len(bot.commands)} Comandos Ativos.")
+        print(f"[OK] - {len(bot.cogs)} Cogs Ativas.")
+
+    def embed(self, ctx, invisible=False):
+        color = self.neutral if invisible else self.cor
+        emb = discord.Embed(color=color)
+        emb.set_footer(text=self.user.name + " © 2020", icon_url=self.user.avatar_url_as())
+        emb.timestamp = ctx.message.created_at
+        return emb
+
+    def erEmbed(self, ctx, error='Erro!'):
+        emb = discord.Embed(title=f':x: | {error}', color=self.ecolor)
+        emb.set_footer(text=self.user.name + " © 2020", icon_url=self.user.avatar_url_as())
+        emb.timestamp = ctx.message.created_at
+        return emb
+
+    #async def on_message(self, message):
+        #return
+
+    def iniciar(self):
+       try:
+           super().run(secrets.TOKEN,reconnect=True)
+       except Exception as e:
+           print(f"Erro ao logar o bot: {e}")
+        
+
+    
+    
+
+
+
+bot = main()
+
+@bot.check
+async def blacklist(ctx):
+    with open('cogs/utils/users_banned.json') as bn:
+        jsn = json.load(bn)
+    if str(ctx.author.id) in jsn and not ctx.author.id == bot.dono:
+        reason = jsn[str(ctx.author.id)]
+        embed = discord.Embed(title=f':x: | Sem permissão!', description=f'Você foi banido de usar qualquer comando meu, o motivo é:\n`{reason}`', color=bot.ecolor)
+        await ctx.send(embed=embed)
+        return False
+    return True
+
+    
+    
+
+
+if __name__ == '__main__':
+    bot = main()
+    bot.iniciar()
+
